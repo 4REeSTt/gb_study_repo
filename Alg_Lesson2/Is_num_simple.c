9c3605b4fcdc74121e4552f8c3f9558404dfcd07
Linus <69402183+4REeSTt@users.noreply.github.com>
2021-04-06 19:16:45 +0500

    Alg 1 and 2 done

---
+++ Alg_Lesson2/Is_num_simple.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+int is_num_simple(int num){
+    for(int i = 2;i < num;i++){
+        if(num % i == 0){
+            printf("Number is not simple\n");
+            return 0;
+        }
+    }
+            printf("Number is simple\n");
+            return 0;
+}
+
+int main(){
+
+    is_num_simple(15);
+    is_num_simple(7);
+    is_num_simple(14);
+
+    
+    return 0;
+}
