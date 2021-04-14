6361b9d392f5cdfb978cf42c0e51e2f195a4c4c0
4REeSTt <yaroslav-pelogin@mail.ru>
2021-04-14 18:24:28 +0300

    Les6 is done with no string(task2)

--- Lesson6/buff_glue.cpp
+++ Lesson6/buff_glue.cpp
@@ -15,29 +15,35 @@ int main(){
     char* prebuff;
     fin.seekg(0, std::ios_base::end);
     size_t file1_size = fin.tellg();
+    fin.seekg(0,fin.beg);
 
 
-//    std::ifstream fin2; 
-//    fin2.open("textholder2.txt");
-//    if(!fin2.is_open()){
-//        std::cerr << "Cant open the file" << std::endl;
-//                        
-//    }
-//    fin2.seekg(0, std::ios_base::end);
-//    size_t file2_size = fin2.tellg();
-//
-//    buff = new char[file2_size + file1_size];
-//    prebuff = new char[file1_size];
+    std::ifstream fin2; 
+    fin2.open("textholder2.txt");
+    if(!fin2.is_open()){
+       std::cerr << "Cant open the file" << std::endl;
+                        
+    }
+    fin2.seekg(0, std::ios_base::end);
+    size_t file2_size = fin2.tellg();
+    fin2.seekg(0,fin2.beg);
+
+    buff = new char[file2_size + file1_size];
+    prebuff = new char[file1_size];
 
 //Копирование данных из файла в буффер 
     fin.read(prebuff,file1_size);
-    for(int i = 0; i < file1_size; i++){
-        std::cout << prebuff[i];
+    fin2.read(buff, file2_size);
+
+    for(int i = file2_size; i < file1_size + file2_size; i++){
+        buff[i] = prebuff[i - file2_size];
     }
-    
 
+    for(int i = 0; i < file1_size + file2_size; i++){
+        std::cout << buff[i];
+    }
 
-    //fin2.close();
+    fin2.close();
     fin.close();
     delete[] prebuff;
 
