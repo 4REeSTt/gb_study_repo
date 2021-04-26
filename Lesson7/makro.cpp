3deddc913a50b65f704a3a2e989ad42777428f9d
Linus <69402183+4REeSTt@users.noreply.github.com>
2021-04-26 13:32:04 +0500

    change dinamic array access

--- Lesson7/makro.cpp
+++ Lesson7/makro.cpp
@@ -2,7 +2,7 @@
 //1.-----------------------------------------------------------------------------------------------------
 #define CHECK_DIAPOSON(number,folow_num) ((number >= 0)&&(number < folow_num))?true:false
 //2.-----------------------------------------------------------------------------------------------------
-#define ARRAY_ELEMENT(i,j) Arr_dinamic[i*M+j]
+#define ARRAY_ELEMENT(Array,i,j) (*(*(Array + i) + j))
 //3.-----------------------------------------------------------------------------------------------------
 #define ARR_SIZE(index,type) sizeof(index)/sizeof(type)
 
@@ -13,13 +13,18 @@ int main(){
     std::cout << (CHECK_DIAPOSON(5,10)) << std::endl;
 
     //2.
-    int * Arr_dinamic,
+    int ** Arr_dinamic,
         N = 5,
         M = 5;
 
-    Arr_dinamic = new int [M*N];
-    Arr_dinamic[19] = 33;
-    std::cout << ARRAY_ELEMENT(3,4) << '\n';
+    Arr_dinamic = new int*[N];
+	Arr_dinamic[0] = new int[M*N];
+	for(int i{0}; i < N; i++){
+		Arr_dinamic[i] = Arr_dinamic[0] + M*i;
+	}
+	
+    Arr_dinamic[2][2] = 33;
+    std::cout << ARRAY_ELEMENT(Arr_dinamic, 2,2) << '\n';
 
     delete[] Arr_dinamic;
 
