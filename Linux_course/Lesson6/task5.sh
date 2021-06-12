ba3373b938ac54a1bd4f32adf369621d69a27aa2
linus <linus@debian>
2021-06-12 13:31:38 -0400

    finished

--- Linux_course/Lesson6/task5.sh
+++ Linux_course/Lesson6/task5.sh
@@ -1,1 +1,2 @@
+# It must work, at -c | tail commant to run is /usr/sbin... , but for some reason my PC didnt shutdown(tried diferent methods with at, didnt work)
 echo '/usr/sbin/shutdown -r now' | at 12:45 
--- Linux_course/Lesson6/task6.sh
+++ Linux_course/Lesson6/task6.sh
@@ -1,1 +1,2 @@
-cp -r /etc /etc.tmp
+# Just write it in crontab -e file and it will make backup of etc for us
+0 5 * * 1 tar -zcf /var/backups/ect.tgz /etc/
