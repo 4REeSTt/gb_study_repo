b6cc2f56567ad23d81043ee2be5899f9b0b41e19
4REeSTt <yaroslav-pelogin@mail.ru>
2021-07-02 18:11:36 +0300

    1task not done

--- Algorithm_course/Alg_Lesson6/main.c
+++ Algorithm_course/Alg_Lesson6/main.c
@@ -5,11 +5,58 @@
 #define STACK_EMPTY (-1)
 
 
+typedef struct{
+    int prior;
+    int data;
+}Node;
 
+Node* arr[STACK_SIZE];
+int max_prior = 0;
 
+void init(){
+    for(int i = 0; i < STACK_SIZE; i++){
+        arr[i] = NULL;
+    }
+}
+
+int position = 0;
+int add_queue(Node* arr, Node node){
+    if(position = STACK_SIZE){
+        printf("queue is full");
+        return -1;
+    }
+    arr[position]  = node;
+    position++;
+    if(max_prior < node.prior)
+        max_prior = node.prior;
+}
+
+int find_next_prior(Node* arr){
+    int tmp = 0;
+    for(int i = 0; i < STACK_SIZE; i++)
+        if(arr[i].prior > tmp)
+            tmp = arr[i].prior;
+    max_prior = tmp;
+} 
+int pop_queue(Node* arr){
+    if(!position){
+        printf("queue is empty");
+        return -1;
+    }
+    for(int i = 0; i < STACK_SIZE; i++){
+        if(arr[i].prior == max_prior){
+            arr[i] = NULL;
+        }
+        find_next_prior(arr);
+        position--;
+    }
+}
+
+//2.--------------------------------------------------------------------
 int Stack[STACK_SIZE];
 int top = -1;
 
+//stack logic
 bool push(int value){
     if(top >= STACK_SIZE) 
         return 0;
@@ -27,6 +74,7 @@ int pop(){
     return result;
 }
 
+//to bin finc--------------------------------------------------
 void tobin(int integer){
     while(integer > 0){
         push(integer % 2);
@@ -37,7 +85,6 @@ void tobin(int integer){
         printf("%d",t);
     } 
     printf("\n");
-
 }
 
 
