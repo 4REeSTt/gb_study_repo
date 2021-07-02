ff4b62c933c3051d86447144da6b58f2627c8415
4REeSTt <yaroslav-pelogin@mail.ru>
2021-07-02 17:28:19 +0300

    stack

--- Algorithm_course/Alg_Lesson6/main.c
+++ Algorithm_course/Alg_Lesson6/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define STACK_SIZE 50
 #define STACK_EMPTY (-1)
@@ -7,18 +8,16 @@
 
 
 int Stack[STACK_SIZE];
-
 int top = -1;
 
 bool push(int value){
     if(top >= STACK_SIZE) 
         return 0;
-
     top++;
     Stack[top] = value;
     return 1;
 }
-    
+
 int pop(){
     if(top <= -1)
         return STACK_EMPTY;
@@ -28,16 +27,24 @@ int pop(){
     return result;
 }
 
+void tobin(int integer){
+    while(integer > 0){
+        push(integer % 2);
+        integer /= 2;
+    }
+    int t;
+    while((t = pop()) != STACK_EMPTY){
+        printf("%d",t);
+    } 
+    printf("\n");
 
+}
 
 
 int main(){
-    push(56);
-    push(78);
-    push(13);
 
-    int t;
-    while((t = pop()) != STACK_EMPTY){
-        printf("%d ",t);
-    }
+    printf("number 5 in binary: ");
+    tobin(5);
+    printf("number 505 in binary: ");
+    tobin(505);
 }
