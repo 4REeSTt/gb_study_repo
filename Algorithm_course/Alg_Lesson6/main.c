8501d32a36c856e6330eb4b36c36fe2f02995286
Linus <69402183+4REeSTt@users.noreply.github.com>
2021-07-06 19:18:00 +0500

    les 6 is done

--- Algorithm_course/Alg_Lesson6/main.c
+++ Algorithm_course/Alg_Lesson6/main.c
@@ -1,55 +1,77 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 #define STACK_SIZE 50
 #define STACK_EMPTY (-1)
 
 
-typedef struct{
-    int prior;
+typedef struct Node{
     int data;
+    int prior;
 }Node;
 
-Node* arr[STACK_SIZE];
+int counter = -1;
 int max_prior = 0;
 
-void init(){
-    for(int i = 0; i < STACK_SIZE; i++){
-        arr[i] = NULL;
-    }
-}
 
-int position = 0;
-int add_queue(Node* arr, Node node){
-    if(position = STACK_SIZE){
-        printf("queue is full");
-        return -1;
+int find_next_prior(Node** arr){
+    if(counter == -1){
+        printf("array is empty");
+        return 0;
     }
-    arr[position]  = node;
-    position++;
-    if(max_prior < node.prior)
-        max_prior = node.prior;
-}
-
-int find_next_prior(Node* arr){
-    int tmp = 0;
-    for(int i = 0; i < STACK_SIZE; i++)
-        if(arr[i].prior > tmp)
-            tmp = arr[i].prior;
-    max_prior = tmp;
-} 
-int pop_queue(Node* arr){
-    if(!position){
-        printf("queue is empty");
-        return -1;
-    }
-    for(int i = 0; i < STACK_SIZE; i++){
-        if(arr[i].prior == max_prior){
-            arr[i] = NULL;
+    max_prior = 0;
+    for(int i = 0; i < 10; i++){
+        if(arr[i] != NULL){
+            if(arr[i]->prior > max_prior){
+                max_prior = arr[i]->prior;            
+            }
         }
-        find_next_prior(arr);
-        position--;
     }
+}
+
+int add_item(Node** arr, Node* node){
+    if(counter == 9){
+        printf("array is full");
+        return 0;
+    }
+    counter++;
+    for(int i = 0; i < 10; i++){
+        if(arr[i] == NULL){
+            arr[i] = node;
+            max_prior = arr[counter]->prior;
+            find_next_prior(arr);
+            return 1;
+        }
+    }
+}
+
+int rm_item(Node** arr){
+    if(counter == -1){
+        printf("array is empty");
+        return 0;
+    }
+    for(int i = 0; i < 10; i++){
+        if(arr[i] == NULL)
+            continue;
+        if(arr[i]->prior == max_prior){
+            arr[i] = NULL;
+            counter--;
+            find_next_prior(arr);
+            return 1;
+        }
+    } 
+}
+
+void print_arr(Node** arr){
+    for(int i = 0; i < 10; i++){
+        if(arr[i] == NULL){
+            printf("[],");
+            continue;
+        }
+        printf("[%d, %d],", arr[i]->prior, arr[i]->data);
+    }
+    printf("\n");
 }
 
 //2.--------------------------------------------------------------------
@@ -89,9 +111,54 @@ void tobin(int integer){
 
 
 int main(){
+	
+	printf("\n");
+    Node* node1 = (Node*)malloc(sizeof(Node));
+    Node* node2 = (Node*)malloc(sizeof(Node));
+    Node* node3 = (Node*)malloc(sizeof(Node));
+    
+    node1->data = 15;
+    node1->prior = 3;
+    node2->data = 20;
+    node3->data = 30;
+    node2->prior = 1;
+    node3->prior = 10;
 
+
+    Node* arr[10];
+    for(int i = 0; i < 10; i++){
+        arr[i] = NULL;
+    }
+
+    add_item(arr, node3);
+    add_item(arr, node1);
+    add_item(arr, node2);
+    add_item(arr, node1);
+    add_item(arr, node3);
+
+    print_arr(arr);
+
+    printf("rm one item\n");
+    rm_item(arr);
+    print_arr(arr);
+
+    printf("rm one item\n");
+    rm_item(arr);
+    print_arr(arr);
+
+
+    printf("add three items\n");
+    add_item(arr, node3);
+    add_item(arr, node2);
+    add_item(arr, node1);
+
+    print_arr(arr);
+    printf("\n");
+
+//2.------------------------------------------------------------
     printf("number 5 in binary: ");
     tobin(5);
     printf("number 505 in binary: ");
     tobin(505);
+	
 }
