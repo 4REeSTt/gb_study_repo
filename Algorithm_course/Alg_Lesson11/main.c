a6e319df24a8510c6f2a06dd21e6a546243166de
4REeSTt <yaroslav-pelogin@mail.ru>
2021-07-22 05:16:55 +0300

    lesson 11 is done

--- Algorithm_course/Alg_Lesson11/main.c
+++ Algorithm_course/Alg_Lesson11/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int key;
-char massage[100];
+int key = 6;
+char massage[100] = "Hello today iam good";
 int i = 0;
 
 
@@ -17,127 +17,182 @@ int get_size_of_str(char* str){
 //1.-------------------------------------------------------------------------------
 
 char* encrypt(char* str){
-    char* new_str = (char*)calloc(0,sizeof(char) * (get_size_of_str(str)));
+    char* new_str = (char*)malloc(sizeof(char) * (get_size_of_str(str)+1));
     while(str[i] != '\0'){
         new_str[i] = str[i] + key;
         i++;
     }
+    new_str[i+1] = '\0';
     i = 0;
     return new_str;
 }
 
 char* decrypt(char* str){
-    char* new_str = (char*)calloc(0,sizeof(char) * (get_size_of_str(str)));
+    char* new_str = (char*)malloc(sizeof(char) * (get_size_of_str(str)+1));
     while(str[i] != '\0'){
         new_str[i] = str[i] - key;
         i++;
     }
+    new_str[i+1] = '\0';
     i = 0;
     return new_str;
 }
 
 //2.-------------------------------------------------------------------------------
 typedef struct Key_str{
-    char** string;
+    char* string;
     int* key;
 }Key_str;
-
-void random_mix(int* arr){
-    int k = rand()%100;
-    for(int i = 0; i < k - 1; i++){
-        int tmp;
-        tmp = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = tmp;
-    }
-}
 
 Key_str* col_change_encrypt(char* str){
 
     Key_str* package = (Key_str*)malloc(sizeof(Key_str));
+    int size = get_size_of_str(str);
+    int col_size;
+    if(size%key != 0)
+        col_size = (size/key)+1;
+    else
+        col_size = size/key;
+
+    package->string = (char*)malloc(sizeof(char) * col_size * key + 1);
+    package->string[col_size*key] = '\0';
+
+    package->key = (int*)malloc(sizeof(int)*key);
     char** matrix = (char**)malloc(sizeof(char*) * key);
     char** matrix_tmp = (char**)malloc(sizeof(char*) * key);
-    int size = get_size_of_str(str);
 
     for(int i = 0; i < key; i++){
-        matrix[i] = (char*)malloc(sizeof(char)* (get_size_of_str(str)/key));
+        matrix[i] = (char*)malloc(sizeof(char)*col_size);
         matrix_tmp[i] = matrix[i];
     }
-    package->string = matrix;
-    package->key = (int*)malloc(sizeof(int)* key);
-    for(int i = 0; i < key; i++)
-        package->key[i] = i;
 
     for(int i = 0; i < key; i++){
-        for(int j = 0; j < key; j++){
-            if((j + i*key) >= size){
-                matrix[i][j] = '/0';
-            } 
-            matrix[i][j] = str[j + i*key];
-        }
-    }
-    exit:
-
-    if(key <= 2)
-        return NULL;
-
-    random_mix(package->key);
-
-    for(int i = 0; i < key; i++){
-        matrix[i] = matrix_tmp[package->key[i]];
-    }
-
-    package->string = matrix;
-    return package;
-}
-
-Key_str* col_change_decrypt(Key_str* package){
-    char** tmp = (char**)malloc(sizeof(char*)* key);
-    for(int i = 0; i < key; i++)
-        tmp[i] = package->string[i];
-    for(int i = 0; i < key; i++){
-        for(int j = 0; j < key; j++){
-            if(i == package->key[j]){
-                package->string[i] = tmp[j];
+        for(int j = 0; j < col_size; j++){
+            if((j +i*col_size) >= size){
+                matrix[i][j] = 'x';
+            }else{
+                matrix[i][j] = str[j + i*col_size];
             }
         }
     }
-    return package;
-}
+    for(int i = 0; i < key; i++)
+        package->key[i] = i;
 
-void print_package_matrix(Key_str* package){
+    for(int i = 0; i < key -1; i++){
+        int tmp;
+        tmp = package->key[i];
+        package->key[i] = package->key[i+1];
+        package->key[i+1] = tmp;
+    }
+
+    
     for(int i = 0; i < key; i++){
         for(int j = 0; j < key; j++){
-            printf("%c", package->string[i][j]);
+           if(i == package->key[j])
+                matrix[i] = matrix_tmp[j];
+        }
+     }
+    
+    int l = 0;
+    for(int j = 0; j < col_size; j++){
+        for(int i = 0; i < key; i++){
+           package->string[l] = matrix[i][j];
+           l++;
         }
     }
+    
+    free(matrix_tmp);
+    for(int i = 0; i < key; i++)
+        free(matrix[i]);
+    free(matrix);
+    
+
+
+    return package; 
 }
+
+char* col_change_decrypt(Key_str* package){
+
+    int size = get_size_of_str(package->string);
+    int col_size = size/key;
+    int l = 0;
+    char* string = (char*)malloc(sizeof(char)* size);
+    string[size-1] = '\0';
+    char** matrix2 = (char**)malloc(sizeof(char*) * key);
+    for(int i = 0; i < key; i++){
+        matrix2[i] = (char*)malloc(sizeof(char)*col_size);
+    }
+    char** matrix_tmp2 = (char**)malloc(sizeof(char*) * key);
+    
+    for(int i = 0; i < col_size; i++){
+        for(int j = 0; j < key; j++){
+            matrix2[j][i] = package->string[l];
+            l++;
+        }
+    }
+    for(int i = 0; i < key; i++)
+            matrix_tmp2[i] = matrix2[i];
+
+    for(int i = 0; i < key; i++){
+        for(int j = 0; j < key; j++){
+            if(i == package->key[j]){
+                matrix2[j] = matrix_tmp2[i];
+            }
+        }
+    }
+    l = 0;
+    for(int i = 0; i < key; i++){
+        for(int j = 0; j < col_size; j++){
+            string[l] = matrix2[i][j];
+            l++;
+        }
+    }
+    string[l] = '\0';
+
+    
+    free(matrix_tmp2);
+    for(int i = 0; i < key; i++)
+        free(matrix2[i]);
+    free(matrix2);
+
+    for(int i = size-1; i > 0; i--){
+        if(string[i] == 'x')
+            string[i] = '\0';
+        else
+            return string;
+    }
+
+    return string;
+}
+
 
 int main(){
 //1.-------------------------------------------------------------------------------
 
-    printf("Enter key:\n> ");
-    scanf("%d", &key);
+    printf("\nKey: %d\n", key);
 
-    printf("Enter massage:\n> ");
-    scanf("%s", &massage);
+    char* encrypted = encrypt(massage);
+    char* decrypted = decrypt(encrypted);
 
-    printf("Encripting: %s\n", encrypt(massage));
-    char* password;
-    password = encrypt(massage);
-    printf("Decripting: %s\n", decrypt(password));
+    printf("Encripting: %s\n", encrypted);
+    printf("Decripting: %s\n", decrypted);
      
-    free(password);
+    free(encrypted);
+    free(decrypted);
 //mem need to be freed after encrypt and decrypt dont use with printf btw
 //2.-------------------------------------------------------------------------------
     
     Key_str* package = col_change_encrypt(massage);
-    print_package_matrix(package);
-    printf("\n");
-    col_change_decrypt(package);
-    print_package_matrix(package);
+    printf("\nEncrypted with col change: ");
+    printf("%s", package->string);
+    char* decrypted_col_str = col_change_decrypt(package);
+    printf("\nDectypted with col change: ");
+    printf("%s\n", decrypted_col_str);
 
-
+    free(decrypted_col_str);
+    free(package->string);
+    free(package->key);
+    free(package);
 
 
 
